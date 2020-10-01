d77b4dd5d92679a9b095b7ef31c5101df1386d12
Lidor Tevet <lidorte@gmail.com>
2020-10-01 13:40:51 +0300

    PatternAction
    Add pattern action for click

---
+++ EscapeRoom/Assets/Scripts/CubeAction.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeAction : MonoBehaviour, ClickAction
+{
+    public void doAction()
+    {
+        print(transform.name);
+    }
+}
---
+++ EscapeRoom/Assets/Scripts/ObjectClicker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectClicker : MonoBehaviour
+{ 
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 10f))
+            {
+                if (hit.transform)
+                {
+                    doAction(hit.transform.gameObject);
+                }
+            }
+        }
+    }
+
+    protected void doAction(GameObject i_gameObject)
+    {
+        ClickAction action = i_gameObject.GetComponent<ClickAction>();
+        if(action != null)
+        {
+            action.doAction();
+        }
+    }
+}
