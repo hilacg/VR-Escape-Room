b28a2bfda4ee54ace1a2f458df18dcd26190c70d
Noam Cohen <noam248@biocatch.com>
2020-10-02 07:48:10 +0300

    Add roomEnv for tests

---
+++ EscapeRoom/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    [SerializeField] public GameObject Door;
+    // Start is called before the first frame update
+    public void Start()
+    {
+        Door.GetComponent<Animator>().enabled = false;
+    }
+
+
+    public void Update()
+    {
+       
+            Door.GetComponent<Animator>().enabled = true;
+    }
+}
