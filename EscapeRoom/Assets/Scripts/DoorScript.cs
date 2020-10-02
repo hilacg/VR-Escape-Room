59899598e270676231760b97ccbed5da028b5383
Noam Cohen <noam248@biocatch.com>
2020-10-02 12:02:24 +0300

    door lock test

--- EscapeRoom/Assets/Scripts/DoorScript.cs
+++ EscapeRoom/Assets/Scripts/DoorScript.cs
@@ -2,19 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoorScript : MonoBehaviour
-{
-    [SerializeField] public GameObject Door;
-    // Start is called before the first frame update
-    public void Start()
-    {
-        Door.GetComponent<Animator>().enabled = false;
-    }
 
-
-    public void Update()
+public class DoorScript : MonoBehaviour, ClickAction
+{ 
+    public void doAction()
     {
-       
-            Door.GetComponent<Animator>().enabled = true;
+        // this.GetComponent<Animator>().enabled = true;
+
+        this.GetComponent<Animator>().SetBool("open", true);
+        //this
     }
 }
