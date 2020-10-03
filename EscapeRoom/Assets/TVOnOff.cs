35f23a231a69add0f1d1d90b26acf47eb9a0761f
Noam Cohen <noam248@biocatch.com>
2020-10-03 21:40:50 +0300

    push the button Tv

---
+++ EscapeRoom/Assets/TVOnOff.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TVOnOff : MonoBehaviour, ClickAction
+{
+    private bool isOn;
+    [SerializeField] public GameObject TV;
+    public void doAction()
+    {
+        isOn = (isOn == true) ? false : true;
+        TV.gameObject.SetActive(isOn);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isOn = false;
+    }
+}
