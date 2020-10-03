27a8be88846ded46a0945e05483c98038ecb7bd2
Lidor Tevet <lidorte@gmail.com>
2020-10-03 20:38:51 +0300

    add player to second room

--- EscapeRoom/Assets/Scripts/Mario/Main/Pipe001Entry.cs
+++ EscapeRoom/Assets/Scripts/Mario/Main/Pipe001Entry.cs
@@ -6,15 +6,9 @@ public class Pipe001Entry : MonoBehaviour
 {
 
     [SerializeField] public GameObject PipeEntry;
-    public int StoodOn;
-    [SerializeField] public GameObject MainCamera;
-    [SerializeField] public GameObject SecondCamera;
-    [SerializeField] public GameObject MainPlayer;
-    [SerializeField] public GameObject FirstStage;
-    [SerializeField] public GameObject Maze;
-    [SerializeField] public GameObject StartSecondPosition;
-
-
+    
+    private int StoodOn;
+   
     void Start()
     {
         PipeEntry.GetComponent<Animator>().enabled = false;
@@ -45,12 +39,6 @@ public class Pipe001Entry : MonoBehaviour
         yield return new WaitForSeconds(2);
         PipeEntry.GetComponent<Animator>().enabled = false;
         PipeEntry.transform.position = new Vector3(0, -1000, 0);
-        SecondCamera.SetActive(true);
-        Maze.SetActive(true);
-        MainCamera.SetActive(false);
-        FirstStage.SetActive(false);
-        Vector3 StartPosition = StartSecondPosition.transform.position;
-        MainPlayer.transform.position = new Vector3(StartPosition.x, StartPosition.y, StartPosition.z);
-
+        Application.LoadLevel(3);
     }
 }
