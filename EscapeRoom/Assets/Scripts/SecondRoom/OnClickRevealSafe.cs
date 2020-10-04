8ddef09acc1ad63d19bc7f1af58e7d8535729ce8
Hilacg <hilagadol@gmail.com>
2020-10-04 22:48:31 +0300

    SecondRoom safe stashed,footprints and code added

--- EscapeRoom/Assets/Scripts/SecondRoom/OnClickRevealSafe.cs
+++ EscapeRoom/Assets/Scripts/SecondRoom/OnClickRevealSafe.cs
@@ -2,22 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Safe : MonoBehaviour,ClickAction
+public class OnClickRevealSafe : MonoBehaviour
 {
-    [SerializeField] 
-    public GameObject safe;
+    [SerializeField]
+    public GameObject poly;
     [SerializeField]
     private Animator _anim;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
-    public void doAction()
+    void OnMouseDown()
     {
-            _anim.SetTrigger("OpenSafe");
-
+        this._anim.SetTrigger("RevealSafe");
+       // _anim.SetTrigger("RevealSafe");
     }
 }
