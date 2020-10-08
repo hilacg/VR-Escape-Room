22bd891b57299151f56c2003621281f6a702c9c9
Noam Cohen <noam.cohen@biocatch.com>
2020-10-08 22:31:47 +0300

    push and destroyyy

--- EscapeRoom/Assets/Scripts/FirstRoom/OnTryOpenKeyHole.cs
+++ EscapeRoom/Assets/Scripts/FirstRoom/OnTryOpenKeyHole.cs
@@ -5,13 +5,12 @@ using UnityEngine;
 public class OnTryOpenKeyHole : MonoBehaviour, ClickAction
 {
     [SerializeField] public GameObject CodeLock;
-    [SerializeField] GameObject canvasKey;
     public void doAction()
     {
         if (FirstRoomCheck.isFoundTheKey())
         {
             CodeLock.SetActive(true);
-            canvasKey.gameObject.SetActive(false);
+            //canvasKey.gameObject.SetActive(false);
         }
     }
 }
