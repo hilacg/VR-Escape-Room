9427b0d5d49a1c0653d07b11b845f615a3ca6c47
Noam Cohen <noam.cohen@biocatch.com>
2020-10-08 17:55:20 +0300

    VRcHANGES

---
+++ EscapeRoom/Assets/GoogleVR/Scripts/UiController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiController : MonoBehaviour
+{
+    public TextMesh text;
+    public TextMesh text1;
+    public Transform man;
+    public Transform cam;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //text.text = Input.GetAxis("Vertical").ToString();
+        //text1.text = Input.GetAxis("Horizontal").ToString();
+
+
+        //foreach (KeyCode kcode in System.Enum.GetValues(typeof(KeyCode)))
+        //{
+        //    if (Input.GetKey(kcode))
+        //    {
+        //        text.text = kcode.ToString();
+        //    }
+        //}
+        //////RaycastHit hit;
+        ////Vector2 vector = new Vector2(Screen.width / 2f, Screen.height / 2f);
+        ////Ray ray = Camera.main.ScreenPointToRay(vector);
+        ////if (Physics.Raycast(ray, out hit))
+        ////{
+
+        //if (kcode.ToString().Contains("Joystick") && kcode.ToString().Contains("Button0"))
+        //{
+        //    if (Input.GetKey(kcode))
+        //    {
+        //        text.text = kcode.ToString();
+        //    }
+        //}
+        //if (hit.transform != null)
+        //{
+        //    //Debug.Log(hit.transform.name.ToString());
+        //    if (hit.transform.GetComponent<KeyPadButton>())
+        //    {
+        //        //hit.transform.GetComponent<KeyPadButton>().OnClick();
+
+        //        //Debug.Log("Hi you pressed on " + hit.transform.name);
+        //    }
+        //}
+    }
+    //foreach (KeyCode kcode in System.Enum.GetValues(typeof(KeyCode)))
+    //{
+    //    if (Input.GetKey(kcode))
+    //        text.text = kcode.ToString();
+
+    //}
+
+}
---
+++ EscapeRoom/Assets/Scripts/characterMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class characterMove : MonoBehaviour {
+    //public static bool moveforward;
+    public float speed = 2F;
+    private CharacterController controller;
+    [SerializeField] private Transform vrHead;
+    private bool moveForword = false;
+    //float startTime = -1;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        // vrHead = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        if(!(Input.GetAxis("Vertical") < 0.1 && Input.GetAxis("Vertical") > - 0.1) || !(Input.GetAxis("Horizontal") < 0.1 && Input.GetAxis("Horizontal") > -0.1)){ //Joystick in use
+            Vector3 moveVector = vrHead.forward * speed;
+            controller.Move(moveVector * Time.deltaTime);
+        }
+    }
+}
