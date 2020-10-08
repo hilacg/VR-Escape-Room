49f008d1953545bbcecf2a5fdb7845617aa90639
Lidor Tevet <lidorte@gmail.com>
2020-10-08 19:16:05 +0300

    change room one

---
+++ EscapeRoom/Assets/Scripts/FirstRoom/ChangeColorInAndOut.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorInAndOut : MonoBehaviour
+{
+    public void colorIn()
+    {
+        GetComponent<Material>().color = Color.green;
+    }
+
+    public void colorOut()
+    {
+        GetComponent<Material>().color = Color.white;
+    }
+}
--- EscapeRoom/Assets/Scripts/FirstRoom/OnKeyFind.cs
+++ EscapeRoom/Assets/Scripts/FirstRoom/OnKeyFind.cs
@@ -5,12 +5,10 @@ using UnityEngine;
 public class OnKeyFind : MonoBehaviour, ClickAction
 {
     [SerializeField] GameObject key;
-    [SerializeField] GameObject canvasKey;
     public void doAction()
     {
         this.gameObject.SetActive(false);
         key.gameObject.SetActive(false);
-        canvasKey.gameObject.SetActive(true);
         FirstRoomCheck.foundKey();
     }
 }
